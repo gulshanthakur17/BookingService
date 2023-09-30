eb5b27d67bb1933a0cc84135732424320881de32
Gulshan Thakur <gulshanthakur205@gmail.com>
2023-09-30 19:06:47 +0530

    Added RABBIT MQ Configuration

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 
 const { BookingController } = require('../../controllers/index');
+// const { createChannel} = require('../../utils/message-queue');
+
+// const channel = await createChannel();
+const bookingController = new BookingController();
 
 
 const router = express.Router();
 
-router.post('/bookings', BookingController.create);
+router.post('/bookings', bookingController.create);
+router.post('/publish', bookingController.sendMessageToQueue);
 
 module.exports = router;
