ea98b72c913a8ba9a3a5628366b0a16eeaa9a5e4
Gulshan Thakur <gulshanthakur205@gmail.com>
2023-09-30 21:47:10 +0530

    Update RABBIT MQ Configuration

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -16,8 +16,16 @@ class BookingController {
     async sendMessageToQueue( req, res)  {
 
         const channel = await createChannel();
-        const data = {message: 'Success'};
-        publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+        const payload = {
+            data: {
+                subject: 'This is a Notification Queue',
+                content: 'Some Queue will Suscribe this',
+                recepientEmail: 'testingmail@gmail.com',
+                notificationTime: '2023-09-30T09:21:00'
+            },
+            service: 'CREATE_TICKET'
+        };
+        publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(payload));
         return res.status(200).json({
             message: 'Successfully published the event'
         });
